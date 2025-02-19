8118f8d11c5664dc68066d79259fcd630f9fdaf3
DV-Axel <axel.cichello@davinci.edu.ar>
2025-02-19 10:25:43 -0300

    Creada la activity y se manda

--- app/src/main/java/com/example/clinicaloscerros/MainActivity.java
+++ app/src/main/java/com/example/clinicaloscerros/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // Referenciar los botones
         Button btnAddPatient = findViewById(R.id.btnAddPatient);
         Button btnListPatients = findViewById(R.id.btnListPatients);
+        Button btnListHospitals = findViewById(R.id.btnListHospitals);
 
         // Listener para el botón "Agregar Paciente"
         btnAddPatient.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Abrir la actividad del listado de pacientes
                 Intent intent = new Intent(MainActivity.this, ListadoPacientesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // Listener para el botón "Hospitales cercanos"
+        btnListHospitals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir la actividad del listado de pacientes
+                Intent intent = new Intent(MainActivity.this, HospitalesCercanosActivity.class);
                 startActivity(intent);
             }
         });
